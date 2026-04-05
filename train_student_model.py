2503068d13e56db31d78e7487f24d467a1697bcc
Prem <premsinghayer270@gmail.com>
2026-04-06 02:12:29 +0530

    Used GradientBoostingClassifier for prediction

--- train_student_model.py
+++ train_student_model.py
@@ -10,3 +10,12 @@ df = pd.read_csv('Student Dataset.csv')
 # 1. Separate Features and Target
 X = df.drop(columns=['Student id', 'Academic status'], errors='ignore')
 y = df['Academic status']
+
+
+# We use learning_rate and max_depth to control how it learns
+model = GradientBoostingClassifier(
+    n_estimators=100, 
+    learning_rate=0.1,  # How much each tree contributes to the final result
+    max_depth=5,        # Keeps the individual trees relatively shallow to prevent overfitting
+    random_state=42
+)
