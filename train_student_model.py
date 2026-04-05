cb56c7b0cf53feb5d1bf700f8cb8a7dbb80b3b60
Prem <premsinghayer270@gmail.com>
2026-04-06 02:11:07 +0530

    Separate Features and Target

--- train_student_model.py
+++ train_student_model.py
@@ -6,3 +6,7 @@ import os
 
 print("Loading dataset...")
 df = pd.read_csv('Student Dataset.csv')
+
+# 1. Separate Features and Target
+X = df.drop(columns=['Student id', 'Academic status'], errors='ignore')
+y = df['Academic status']
