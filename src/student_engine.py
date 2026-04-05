45aaca91d0b5c06bc310b4d138d1b8be4bc1d66d
Prem <premsinghayer270@gmail.com>
2026-04-06 02:19:53 +0530

    Features added inside the class

--- src/student_engine.py
+++ src/student_engine.py
@@ -5,4 +5,11 @@ class StudentPredictor:
     def __init__(self):
         try:
             self.model = joblib.load('models/student_performance_model.pkl')
-            
+
+            self.features = [
+                'previous_semester_sgpa', 
+                'Attendance marks', 
+                'assignment_score_average(out of 20)',
+                'midterm_exam_avg', 
+                'Lowest midterm_exam_score'
+            ]
