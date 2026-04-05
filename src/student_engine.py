fef4e2703ddbbc40ae3253d03ad70d54a4c6c110
Prem <premsinghayer270@gmail.com>
2026-04-06 02:19:02 +0530

    constructor made for StudentPredictor

--- src/student_engine.py
+++ src/student_engine.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import joblib
+
+class StudentPredictor:
+    def __init__(self):
+        try:
+            self.model = joblib.load('models/student_performance_model.pkl')
+            
